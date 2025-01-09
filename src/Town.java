0f2ac3f71310f128b3f8fab6d62c925457c1dcd9
BT_2S14_20 <danielk356@NYCSTUDENTS.NET>
2025-01-09 10:29:48 -0500

    I fixed how the gold is displayed.

--- src/Town.java
+++ src/Town.java
@@ -147,7 +147,7 @@ public class Town
                 hunter.changeGold(goldDiff);
             } else {
                 printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
-                printMessage += "\nYou lost the brawl and pay " + goldDiff + " gold.";
+                printMessage += "\nYou lost the brawl and pay " + goldLost + " gold.";
                 hunter.changeGold(-1 * goldLost);
             }
         }
